7c4fef7fbd844f58ea8270142a455525bd7726b5
nirch <nirh2@yahoo.com>
2021-01-03 12:05:50 +0200

    removing the redundant resultFull + adding layout in ActorsPage

--- src/pages/ActorsPage.js
+++ src/pages/ActorsPage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react"
-import { Container } from "react-bootstrap";
+import { CardDeck, CardGroup, Col, Container, Row } from "react-bootstrap";
 import ActorCard from "../components/ActorCard/ActorCard";
 import SearchBox from "../components/SearchBox/SearchBox";
 import ActorModel from "../model/ActorModel";
@@ -10,7 +10,6 @@ function ActorsPage() {
     const [actors, setActors] = useState([]);
     const [searchText, setSearchText] = useState("");
     const [results, setResults] = useState([]);
-    const [resultsFull, setResultsFull] = useState([]);
 
 
     function searchTextChange(newSearchText) {
@@ -19,8 +18,7 @@ function ActorsPage() {
         // update results is search is not empty
         if (newSearchText) {
             axios.get("https://api.themoviedb.org/3/search/person?api_key=53d2ee2137cf3228aefae083c8158855&query=" + newSearchText).then(res => {
-                setResults(res.data.results.map(result => result.name));
-                setResultsFull(res.data.results);
+                setResults(res.data.results);
             })
         } else {
             setResults([]);
@@ -29,18 +27,20 @@ function ActorsPage() {
 
     function addActor(index) {
         // alert(results[index]);
-        setActors(actors.concat(new ActorModel(resultsFull[index].name, resultsFull[index].profile_path)));
+        setActors(actors.concat(new ActorModel(results[index].name, results[index].profile_path)));
         setSearchText("");
         setResults([]);
     }
 
-    const actorsView = actors.map((actor, index) => <ActorCard key={index} actor={actor}/>)
+    const actorsView = actors.map((actor, index) => <Col key={index} lg={3} md={6}><ActorCard actor={actor}/></Col>)
     return (
         <div className="p-actors">
             <Container>
                 <SearchBox placeholder="Add actor..." value={searchText} onSearchChange={searchTextChange}
-                    results={results} onResultSelected={addActor}/>
-                {actorsView}
+                    results={results.map(result => result.name)} onResultSelected={addActor}/>
+                <Row>
+                    {actorsView}
+                </Row>
             </Container>
         </div>
     )
